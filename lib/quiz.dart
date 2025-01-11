909b1ca469ec8b8b3c74725b53417be6e96d33aa
ikhsan <ikhsanmuhamad3885@gmail.com>
2025-01-12 00:09:08 +0700

    add: button switch page

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/questions_screen.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -11,17 +12,34 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  Widget? activeScreen;
+
+  @override
+  void initState() {
+    activeScreen = StartScreen(switchScreen);
+    super.initState();
+  }
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = QuestionsScreen();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          body: Container(
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(colors: [
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
                 Color.fromARGB(255, 42, 10, 118),
                 Color.fromARGB(255, 72, 9, 101)
-              ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
-              child: StartScreen())),
+            ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
+          ),
+          child: activeScreen,
+        ),
+      ),
     );
   }
 }
